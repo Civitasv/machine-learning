3771c512be3f76a9f48c038ee6ab6d14467515f8
Civitasv <2309780282@qq.com>
2021-11-23 15:40:16 +0800

    week 6 program assignment finished

--- week 6/ex5/linearRegCostFunction.m
+++ week 6/ex5/linearRegCostFunction.m
@@ -19,15 +19,9 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
+J = 1./(2.*m) .* (sum((X * theta - y).^2) + lambda .* sum(theta(2:end).^2));
+grad(1) = 1./m .* (X * theta - y)' * X(:,1);
+grad(2:end) = 1./m .* (X*theta - y)' * X(:, 2:end) + lambda./m .* theta(2:end)';
 
 
 % =========================================================================
